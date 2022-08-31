0a705184f4b468cbb76310a443a734b56b5a00c4
André Gross <andgross@gmail.com>
2022-08-31 16:12:38 -0300

    Update: Requisito #05, ok

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,65 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    // isLoading: false,
+    searchArtist: '',
+    isSearchButtonDisabled: true,
+    // isRedirect: false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState(
+      {
+        searchArtist: value,
+      },
+      () => this.handleButtonDisabled(),
+    );
+  };
+
+  handleButtonDisabled = () => {
+    const { searchArtist } = this.state;
+    const maxLength = 2;
+    if (searchArtist.length >= maxLength) {
+      this.setState({
+        isSearchButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSearchButtonDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isSearchButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-field">
+            Nome
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="search"
+              id="search-field"
+              onChange={ this.onInputChange }
+            />
+          </label>
+          <div>
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              name="submit-button"
+              disabled={ isSearchButtonDisabled }
+              // onClick={ this.handleSaveLogin }
+            >
+              Pesquisar
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
